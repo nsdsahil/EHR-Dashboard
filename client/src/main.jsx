7a643a0e59906c1e3a570d9088aee1f866db45be
Sahil Nishad <sahilroyal91@gmail.com>
2024-02-22 16:06:13 +0530

    client and Server code uploaded

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
 import AuthContextProvider from "./context/AuthContextProvider.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraProvider>
-    <BrowserRouter>
-    <AuthContextProvider>
-   
-    <App />
-    </AuthContextProvider>
-    </BrowserRouter>
-    </ChakraProvider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<ChakraProvider>
+			<BrowserRouter>
+				<AuthContextProvider>
+					<App />
+				</AuthContextProvider>
+			</BrowserRouter>
+		</ChakraProvider>
+	</React.StrictMode>
+);
